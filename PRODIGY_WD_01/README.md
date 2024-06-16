d15c92dfe1a1ce1df38ec271aa1b90a540dd4445
Tinku M. Kaushik <139141786+tinkukaushik@users.noreply.github.com>
2024-06-16 13:01:36 +0530

    Add files via upload

---
+++ PRODIGY_WD_01/README.md
@@ -0,0 +1,2 @@
+# PRODIGY_WD_01
+ it is aninternship project numer 1 , based on HTML ,CSS , JS
---
+++ PRODIGY_WD_01/script.js
@@ -0,0 +1,10 @@
+// script.js
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
